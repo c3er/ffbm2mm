32911de70a760fdbfdd280b4ee441629f4d0eed4
Christian Dreier <flipper.x3r@googlemail.com>
2016-02-04 02:33:38 +0100

    First version - not working yet

---
+++ doc/t.mm
@@ -0,0 +1,72 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Test" ID="ID_1924660900" CREATED="1454547684269" MODIFIED="1454547684269"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Das ist ein Test" POSITION="right" ID="ID_443933135" CREATED="1454547684269" MODIFIED="1454547684269"/>
+<node TEXT="Noch ein Test" POSITION="right" ID="ID_688611485" CREATED="1454547684269" MODIFIED="1454547684269"/>
+<node TEXT="Ein Link" POSITION="right" ID="ID_1697945215" CREATED="1454547684269" MODIFIED="1454547684269" LINK="https://docs.python.org/3.5/library/json.html"/>
+<node TEXT="https://docs.python.org/3.5/library/json.html" POSITION="right" ID="ID_1119714343" CREATED="1454547684269" MODIFIED="1454547684269" LINK="https://docs.python.org/3.5/library/json.html"/>
+<node TEXT="https://www.google.de/" POSITION="right" ID="ID_479722414" CREATED="1454547684269" MODIFIED="1454547684269" LINK="https://www.google.de/">
+<node TEXT="Direkte Adreese und mit Kind" ID="ID_828621937" CREATED="1454547684269" MODIFIED="1454547684269"/>
+</node>
+<node TEXT="http://effbot.org/tkinterbook/button.htm" POSITION="right" ID="ID_1012382323" CREATED="1454547684269" MODIFIED="1454547684269" LINK="http://effbot.org/tkinterbook/button.htm"/>
+<node TEXT="Jetzt mit Hierarchie" POSITION="right" ID="ID_609829106" CREATED="1454547684269" MODIFIED="1454547684269">
+<node TEXT="1. Kind" ID="ID_323771902" CREATED="1454547684269" MODIFIED="1454547684269"/>
+<node TEXT="2. Kind" ID="ID_1287637900" CREATED="1454547684269" MODIFIED="1454547684269">
+<node TEXT="1. Kindkind" ID="ID_417352350" CREATED="1454547684269" MODIFIED="1454547684269">
+<node TEXT="Kindkindkind" ID="ID_685520787" CREATED="1454547684269" MODIFIED="1454547684269"/>
+</node>
+<node TEXT="2. Kindkind" ID="ID_1443543637" CREATED="1454547684269" MODIFIED="1454547684269"/>
+</node>
+<node TEXT="3. Kind" ID="ID_685811017" CREATED="1454547684269" MODIFIED="1454547684269"/>
+</node>
+<node TEXT="sdsdsd" POSITION="right" ID="ID_1030126593" CREATED="1454547697284" MODIFIED="1454547698796"/>
+</node>
+</map>
--- ffbm2mm/ffbm2mm.py
+++ ffbm2mm/ffbm2mm.py
@@ -4,3 +4,87 @@
 """Transform Firefox bookmarks in JSON format to a mind map in Freeplane
 format.
 """
+
+
+import sys
+import os
+import json
+
+
+class MMNode:
+    def __init__(self, json_obj, is_root=True):
+        self._json_obj = json_obj
+        self.is_root = is_root
+        self.text = json_obj["title"]
+        self.link = self.get_link(json_obj)
+        self.children = self.get_children(json_obj)
+
+    def dump(self):
+        nodestr = '<node TEXT="{}"'.format(self.text)
+
+        if self.link:
+            nodestr += ' LINK="{}"'.format(self.link)
+
+        if self.children:
+            nodestr += '>\n'
+            childlist = []
+            for child in self.children:
+                childlist.append(child.dump())
+            children = "\n".join(childlist)
+            nodestr += "\n".join((children, '</node>'))
+        else:
+            nodestr += '/>'
+
+        if self.is_root:
+            return "\n".join((
+                '<map version="freeplane 1.2.0">',
+                nodestr,
+                '</map>'
+            ))
+        return nodestr
+
+    @staticmethod
+    def get_link(obj):
+        try:
+            return obj["uri"]
+        except KeyError:
+            return None
+
+    @staticmethod
+    def get_children(obj):
+        children = []
+        try:
+            json_children = obj["children"]
+            for json_child in json_children:
+                children.append(MMNode(json_child, False))
+        except KeyError:
+            pass
+        return children
+
+
+def error(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_args(args):
+    if len(args) != 3:
+        error("Parameters: <bookmarks file name> <mind map file name>")
+    bookmarks_fname = args[1]
+    mm_fname = args[2]
+    if not os.path.exists(bookmarks_fname):
+        error("Could not find bookmarks file: " + bookmarks_fname)
+    return bookmarks_fname, mm_fname
+
+
+def main(args):
+    bookmarks_fname, mm_fname = parse_args(args)
+    with open(bookmarks_fname) as f:
+        bookmarks = json.load(f)
+    mm = MMNode(bookmarks)
+    with open(mm_fname, "w") as f:
+        f.write(mm.dump())
+
+
+if __name__ == "__main__":
+    main(sys.argv)
